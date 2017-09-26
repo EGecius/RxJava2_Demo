2691f6a73e6cd5c8a7fde665e4a85151f3193390
Egis <e.gecius@gmail.com>
2017-09-26 20:30:06 +0100

    add more examples for startsWith

--- app/src/main/java/com/egecius/rxjava2_demo_2/rx/startWith/StartWithExamples.java
+++ app/src/main/java/com/egecius/rxjava2_demo_2/rx/startWith/StartWithExamples.java
@@ -1,13 +1,31 @@
 package com.egecius.rxjava2_demo_2.rx.startWith;
 
+import static android.R.id.list;
+
 import java.util.List;
 
 import io.reactivex.Observable;
 
 public class StartWithExamples {
 
-    public Observable<Integer> startWith(List<Integer> list) {
+    public Observable<Integer> startWithObservable(List<Integer> list) {
+        Observable<Integer> observable = Observable.fromIterable(list);
         return Observable.just(1, 2, 3)
-                .startWith(Observable.fromIterable(list));
+                .startWith(observable);
+    }
+
+    public Observable<Integer> startWithIterable(Iterable<Integer> iterable) {
+        return Observable.just(1, 2, 3)
+                .startWith(iterable);
+    }
+
+    public Observable<Integer> startWithArray(Integer... integers) {
+        return Observable.just(1, 2, 3)
+                .startWithArray(integers);
+    }
+
+    public Observable<Integer> startWithElement(Integer integer) {
+        return Observable.just(1, 2, 3)
+                .startWith(integer);
     }
 }
--- app/src/test/java/com/egecius/rxjava2_demo_2/rx/startWith/StartWithExamplesTest.java
+++ app/src/test/java/com/egecius/rxjava2_demo_2/rx/startWith/StartWithExamplesTest.java
@@ -1,5 +1,7 @@
 package com.egecius.rxjava2_demo_2.rx.startWith;
 
+import static android.R.id.list;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,15 +23,49 @@ public class StartWithExamplesTest {
     }
 
     @Test
-    public void startWithAppendsValues() {
+    public void startWithObservable() {
         List<Integer> list = Arrays.asList(3, 14);
 
-        TestObserver<Integer> testObserver = mSut.startWith(list).test();
+        TestObserver<Integer> testObserver = mSut.startWithObservable(list).test();
 
         testObserver
                 .assertNoErrors()
                 .assertComplete()
                 .assertValues(3, 14, 1, 2, 3);
+    }
+
+    @Test
+    public void startWithIterable() {
+        List<Integer> list = Arrays.asList(3, 14);
+
+        TestObserver<Integer> testObserver = mSut.startWithIterable(list).test();
+
+        testObserver
+                .assertNoErrors()
+                .assertComplete()
+                .assertValues(3, 14, 1, 2, 3);
+    }
+
+    @Test
+    public void startWithArray() {
+        Integer[] array = {3, 14};
+
+        TestObserver<Integer> testObserver = mSut.startWithArray(array).test();
+
+        testObserver
+                .assertNoErrors()
+                .assertComplete()
+                .assertValues(3, 14, 1, 2, 3);
+    }
+
+    @Test
+    public void startWithElement() {
+        TestObserver<Integer> testObserver = mSut.startWithElement(14).test();
+
+        testObserver
+                .assertNoErrors()
+                .assertComplete()
+                .assertValues(14, 1, 2, 3);
     }
 
 }
