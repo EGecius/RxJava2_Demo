e3c4c57d804e31f51aabf9fcda35a91d5e7b1c86
Egidijus Gecius <e.gecius@gmail.com>
2017-09-29 11:36:57 +0100

    show how missing error handling crashes the app on Android

--- app/src/main/java/com/egecius/rxjava2_demo_2/MainActivity.java
+++ app/src/main/java/com/egecius/rxjava2_demo_2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.egecius.rxjava2_demo_2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import io.reactivex.Observable;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+
+        emitErrorWithoutOnErrorHandling();
+    }
+
+    /** This will crash the app because rxJava 2 not only prints the stracktrace but also
+     * forwards uncaught exception to current thread's error handler. On Android this is very
+     * strict and crashes the app */
+    private void emitErrorWithoutOnErrorHandling() {
+        Observable.error(new RuntimeException())
+                .subscribe();
+    }
 }
