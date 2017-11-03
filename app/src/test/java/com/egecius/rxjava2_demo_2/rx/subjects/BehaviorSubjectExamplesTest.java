c7e9ba817a80c3750d17b07763e29b91c1850c05
Egidijus Gecius <e.gecius@gmail.com>
2017-11-03 10:55:33 +0000

    add BehaviourSubject examples

---
+++ app/src/test/java/com/egecius/rxjava2_demo_2/rx/subjects/BehaviorSubjectExamplesTest.java
@@ -0,0 +1,88 @@
+package com.egecius.rxjava2_demo_2.rx.subjects;
+
+import static java.util.Arrays.asList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import io.reactivex.observers.TestObserver;
+import io.reactivex.subjects.BehaviorSubject;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BehaviorSubjectExamplesTest {
+
+    private BehaviorSubjectExamples mSut;
+    private static final Exception EXCEPTION = new Exception();
+
+    @Before
+    public void setUp() {
+        mSut = new BehaviorSubjectExamples();
+    }
+
+    @Test
+    public void cachesLastKnownValue() {
+        BehaviorSubject<Integer> subject = mSut.emit(asList(1, 2, 3));
+
+        TestObserver<Integer> testObserver = subject.test();
+
+        testObserver
+                .assertNotTerminated()
+                .assertValue(3);
+    }
+
+    @Test
+    public void whenSubscribedCompletedSubjectNoEventsAreEmitted() {
+        BehaviorSubject<Integer> subject = mSut.emitWithOnComplete(asList(1, 2, 3));
+
+        TestObserver<Integer> testObserver = subject.test();
+
+        testObserver
+                .assertComplete()
+                .assertNoValues();
+    }
+
+    @Test
+    public void whenSubscribedToFailedSubjectNoEventsAreEmitted() {
+        BehaviorSubject<Integer> subject = mSut.emitWithError(asList(1, 2, 3), EXCEPTION);
+
+        TestObserver<Integer> testObserver = subject.test();
+
+        testObserver
+                .assertError(EXCEPTION)
+                .assertNoValues();
+    }
+
+    @Test
+    public void whenSubscribesBeforeOnCompleteReceivesPriorEvents() {
+        BehaviorSubject<Integer> subject = mSut.emit(asList(1, 2, 3));
+
+        TestObserver<Integer> testObserver = subject.test();
+
+        subject.onNext(14);
+        subject.onNext(15);
+        subject.onComplete();
+
+        testObserver
+                .assertComplete()
+                .assertValues(3, 14, 15);
+    }
+
+    @Test
+    public void whenSubscribesBeforeErrorReceivesPriorEvents() {
+        BehaviorSubject<Integer> subject = mSut.emit(asList(1, 2, 3));
+
+        TestObserver<Integer> testObserver = subject.test();
+
+        subject.onNext(14);
+        subject.onNext(15);
+        subject.onError(EXCEPTION);
+
+        testObserver
+                .assertValues(3, 14, 15)
+                .assertNotComplete()
+                .assertError(EXCEPTION);
+    }
+
+}
