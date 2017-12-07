51dd35b56bcf6b32a8cb9c5d9f752c0b105ce712
Egidijus Gecius <e.gecius@gmail.com>
2017-12-07 19:26:56 +0000

    add testing utils

---
+++ app/src/main/java/com/egecius/rxjava2_demo_2/rx/utils/UncaughtException.java
@@ -0,0 +1,7 @@
+package com.egecius.rxjava2_demo_2.rx.utils;
+
+public class UncaughtException extends RuntimeException {
+    public UncaughtException(Throwable throwable) {
+        super(throwable);
+    }
+}
