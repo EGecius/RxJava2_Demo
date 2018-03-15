d7746bc730f888b42bb2e1d922b33bd20bcd03e5
Egidijus Gecius <>
2018-03-15 14:37:16 +0000

    have all tests pass in CompletableSubjectExamplesTest

--- app/src/test/java/com/egecius/rxjava2_demo_2/rx/subjects/CompletableSubjectExamplesTest.java
+++ app/src/test/java/com/egecius/rxjava2_demo_2/rx/subjects/CompletableSubjectExamplesTest.java
@@ -1,14 +1,15 @@
 package com.egecius.rxjava2_demo_2.rx.subjects;
 
+import static com.egecius.rxjava2_demo_2.rx.utils.TestingUtils.setUncaughtExceptionsHandlerAsStrict;
+
 import com.egecius.rxjava2_demo_2.rx.EgisException;
+import com.egecius.rxjava2_demo_2.rx.utils.UncaughtException;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import io.reactivex.exceptions.UndeliverableException;
 import io.reactivex.observers.TestObserver;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -18,11 +19,11 @@ public class CompletableSubjectExamplesTest {
 
     @Before
     public void setUp() {
+        setUncaughtExceptionsHandlerAsStrict();
         mSut = new CompletableSubjectExamples();
     }
 
-    @Ignore // crashes - not sure why it does not catch UndeliverableException
-    @Test (expected = UndeliverableException.class)
+    @Test (expected = UncaughtException.class)
     public void failsWhenTwoErrorsEmitted() {
         TestObserver<Void> testObserver = mSut.getCompletableSubject().test();
         mSut.emit2Errors();
