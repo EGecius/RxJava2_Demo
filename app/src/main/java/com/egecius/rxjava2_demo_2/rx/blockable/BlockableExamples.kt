2a975e766f19c94de39101ab71dc07b90ed86d11
Egidijus Gecius <e.gecius@gmail.com>
2018-04-12 17:43:54 +0100

    simplify BlockableExamples

--- app/src/main/java/com/egecius/rxjava2_demo_2/rx/blockable/BlockableExamples.kt
+++ app/src/main/java/com/egecius/rxjava2_demo_2/rx/blockable/BlockableExamples.kt
@@ -3,7 +3,6 @@ package com.egecius.rxjava2_demo_2.rx.blockable
 
 import io.reactivex.Observable
 import io.reactivex.Single
-import java.util.*
 
 class BlockableExamples {
 
@@ -26,17 +25,6 @@ class BlockableExamples {
     fun blockingGetForObservable(): Int? {
         return Observable.just(doNetworkCall())
                 .blockingFirst()
-    }
-
-    companion object {
-
-        fun <E> makeList(iter: Iterable<E>): List<E> {
-            val list = ArrayList<E>()
-            for (item in iter) {
-                list.add(item)
-            }
-            return list
-        }
     }
 
 }
