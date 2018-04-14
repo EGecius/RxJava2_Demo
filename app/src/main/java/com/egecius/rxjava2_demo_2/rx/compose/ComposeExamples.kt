a364fd369197b2622eabd78c14587631b0af9b7e
Egidijus Gecius <e.gecius@gmail.com>
2018-04-14 10:38:38 +0100

    convert to ComposeExamples

---
+++ app/src/main/java/com/egecius/rxjava2_demo_2/rx/compose/ComposeExamples.kt
@@ -0,0 +1,38 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
+package com.egecius.rxjava2_demo_2.rx.compose
+
+import io.reactivex.Observable
+import io.reactivex.ObservableTransformer
+
+class ComposeExamples {
+
+    internal fun doubleAndConvertToString(item: Int): Observable<String> {
+        return Observable
+                .just(item)
+                .compose(doubleAndConvertToString())
+    }
+
+    fun doubleAndConvertToString(): ObservableTransformer<Int, String> {
+        return ObservableTransformer { upstream ->
+            upstream
+                    .map { it * 2 }
+                    .map { it.toString() }
+        }
+    }
+
+    internal fun doubleAndConvertToStringRepeatedThrice(item: Int): Observable<String> {
+        return Observable
+                .just(item)
+                .compose(doubleAndConvertToString())
+                .compose(repeatThrice())
+    }
+
+    private fun repeatThrice(): ObservableTransformer<String, String> {
+        return ObservableTransformer { upstream ->
+            upstream
+                    .map { string -> string + string + string }
+        }
+    }
+
+}
