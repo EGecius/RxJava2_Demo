4f2bc43eb4bf267129b6a80da3ba07565a560f12
Egidijus Gecius <e.gecius@gmail.com>
2018-04-26 17:25:21 +0100

    simplify RangeExampleTest

--- app/src/test/java/com/egecius/rxjava2_demo_2/rx/range/RangeExampleTest.kt
+++ app/src/test/java/com/egecius/rxjava2_demo_2/rx/range/RangeExampleTest.kt
@@ -2,20 +2,9 @@ package com.egecius.rxjava2_demo_2.rx.range
 
 import io.reactivex.Flowable
 import io.reactivex.Observable
-import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.junit.MockitoJUnitRunner
 
-@RunWith(MockitoJUnitRunner::class)
 class RangeExampleTest {
-
-    private var mSut: RangeExample? = null
-
-    @Before
-    fun setUp() {
-        mSut = RangeExample()
-    }
 
     @Test
     fun rangeObservable() {
@@ -23,10 +12,7 @@ class RangeExampleTest {
         val testObserver = Observable.range(5, 3).test()
 
         testObserver
-                .assertComplete()
-                .assertComplete()
-                .assertValues(5, 6, 7)
-
+                .assertResult(5, 6, 7)
     }
 
     @Test
@@ -35,10 +21,7 @@ class RangeExampleTest {
         val testSubscriber = Flowable.range(5, 3).test()
 
         testSubscriber
-                .assertComplete()
-                .assertComplete()
-                .assertValues(5, 6, 7)
-
+                .assertResult(5, 6, 7)
     }
 
 }
