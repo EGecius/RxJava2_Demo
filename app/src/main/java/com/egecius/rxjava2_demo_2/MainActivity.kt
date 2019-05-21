af60c4a26aa55d84c61a80c1d0f43b1d09c2b2d8
Egidijus Gecius <e.gecius@gmail.com>
2019-05-21 07:27:43 +0100

    migrate to Android X

--- app/src/main/java/com/egecius/rxjava2_demo_2/MainActivity.kt
+++ app/src/main/java/com/egecius/rxjava2_demo_2/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.egecius.rxjava2_demo_2
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
